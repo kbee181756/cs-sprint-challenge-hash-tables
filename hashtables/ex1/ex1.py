829a0cfe83a79f3855a172d8a43de34411111d3f
Keino Baird <keinobaird@keinos-air.myfiosgateway.com>
2020-11-08 23:36:30 -0500

    hashtables

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Create a cache
+    cache = {}
 
+    # check list of w, to find index
+    for i in range(length):
+        cache[weights[i]] = i
+    
+    # get the w that matches
+    for index, weight in enumerate(weights):
+        wgt = limit - weight
+        if wgt in cache:
+            i = cache[wgt]
+            return (index, i) if index > i else (i, index)
+    
     return None
